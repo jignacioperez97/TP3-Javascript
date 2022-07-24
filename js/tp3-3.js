296efc872d28e97f69cd82febd076ae525a8b289
Ignacio <jignacioperez97@gmail.com>
2022-07-24 16:02:43 -0300

    tp3 punto 3 hecho

---
+++ js/tp3-3.js
@@ -0,0 +1,19 @@
+/* 3- Escribir un script que simule el lanzamiento de dos dados. 
+Hacer uso de la función Math.random para obtener números aleatorios entre 1 y 6 para cada uno de los lanzamientos de los dados. 
+Sumar el resultado de lanzar dos dados y anotar en un array el número de apariciones de dicha suma, repitiendo 50 veces esta operación. */
+
+let resultadosDados = [];
+
+document.write(`Resultados de dados:`);
+for (let i = 0; i < 50; i++) {
+  dado1 = Math.floor(Math.random() * 6 + 1);
+  dado2 = Math.floor(Math.random() * 6 + 1);
+  let sumaDados = dado1 + dado2;
+
+  resultadosDados.push(sumaDados);
+  document.write(" ");
+  document.write(`${resultadosDados[i]}`);
+  document.write(" ,");
+}
+
+document.write(`<br>`);
