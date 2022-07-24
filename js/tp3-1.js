5ac92072faf867e37e5202abfc659baf22e17dc9
Ignacio <jignacioperez97@gmail.com>
2022-07-24 13:09:05 -0300

    tp3 punto 1 hecho

---
+++ js/tp3-1.js
@@ -0,0 +1,25 @@
+//1- Crear un array llamado meses y que almacene el nombre de los doce meses del año.
+// Mostrar por pantalla en forma de lista  los doce nombres del arreglo.
+
+let meses = [
+  "Enero",
+  "Febrero",
+  "Marzo",
+  "Abril",
+  "Mayo",
+  "Junio",
+  "Julio",
+  "Agosto",
+  "Septiembre",
+  "Octubre",
+  "Noviembre",
+  "Diciembre",
+];
+
+document.write("<h3>Meses:</h3>");
+document.write(`<ul>`);
+for (let indiceMeses = 0; indiceMeses < meses.length; indiceMeses++) {
+  document.write(`<li> ${meses[indiceMeses]}</li>`);
+}
+
+document.write(`</ul>`);
